491f9f83daa54f44640c4a59a5fb5ad9be57f68c
fellix <matuhin.maxim@gmail.com>
2015-09-30 08:58:38 +0900

    header title of page move into separate file for using in other controllers

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -6,7 +6,9 @@ class IndexController extends CController {
     public function actionIndex() {
         
         $this->model = new RunModel();
-        
+        $this->data['phead'] = "Монитор проходной НГРЭС";
+        $this->render('../info', false);
+
         //var_dump($this->model);
         $this->render('menu');
         
--- views/hcommon.php
+++ views/hcommon.php
@@ -40,6 +40,4 @@
 
         <!-- Begin page content -->
         <div class="container">
-            <div class="page-header">
-                <h1>Sticky footer with fixed navbar</h1>
-            </div>
+
