d001310707f490ca367935eb9b14c31a5498d2d6
fellix <matuhin.maxim@gmail.com>
2015-10-01 21:25:39 +0900

    wanna best

--- views/monitor/filter.php
+++ views/monitor/filter.php
@@ -2,23 +2,78 @@
 
     <div class="panel panel-success">
         <div class="panel-heading">
-            <i class="glyphicon glyphicon-search"></i>
-            <strong>ФИЛЬТР</strong>
+            <i class="glyphicon glyphicon-search"></i>&nbsp;
+            <strong>Настройка фильтра</strong>
         </div>
         <div class="panel-body">
-            <div class="col-xs-3">
-                <input type="text" class="form-control" placeholder="ФИО сотрудника">
+
+            <div class="col-xs-12 col-md-5">
+                <div class="row form-group">
+                    <label class="col-xs-12" for="">Дата :</label>
+                    <div class="col-xs-6">
+                        <div class="input-group">
+                            <span class="input-group-addon">
+                                <i class="glyphicon glyphicon-calendar" title="С"></i>
+                            </span>
+                            <input type="text" class="form-control" />
+                        </div>
+                    </div>
+                    <div class="col-xs-6">
+                        <div class="input-group">
+                            <span class="input-group-addon" title="По">
+                                <i class="glyphicon glyphicon-calendar"></i>
+                            </span>
+                            <input type="text" class="form-control" />
+                        </div>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <label class="col-xs-12" for="">Время :</label>
+                    <div class="col-xs-6">
+                        <div class="input-group">
+                            <span class="input-group-addon">
+                                <i class="glyphicon glyphicon-time" title="С"></i>
+                            </span>
+                            <input type="text" class="form-control" />
+                        </div>
+                    </div>
+                    <div class="col-xs-6">
+                        <div class="input-group">
+                            <span class="input-group-addon">
+                                <i class="glyphicon glyphicon-time" title="По"></i>
+                            </span>
+                            <input type="text" class="form-control" />
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-xs-3">
-                <input type="text" class="form-control" placeholder="Табельный номер">
+
+            <div class="col-xs-12 col-md-7">
+
+                <div class="row form-group">
+                    <label for="" class="col-xs-12">Сотрудник :</label>
+                    <div class="col-xs-12">
+                        <input type="" class="form-control" placeholder="Фамилия" />
+                    </div>
+                    <div class="col-xs-6">
+                        <input type="" class="form-control" placeholder="Имя"/>
+                    </div>
+                    <div class="col-xs-6">
+                        <input type="" class="form-control" placeholder="Отчество"/>
+                    </div>
+                </div>
+                
+                <div class="row form-group">
+                    <div class="col-xs-6">
+                        <input type="text" class="form-control" placeholder="Цех"/>
+                    </div>
+                    <div class="col-xs-6">
+                        <input type="text" class="form-control" placeholder="Табельный номер"/>
+                    </div>
+                </div>
+
             </div>
-            <div class="col-xs-3">
-                <input type="text" class="form-control" placeholder="Цех">
-            </div>
-            <div class="col-xs-3">
-                <input type="text" class="form-control" placeholder="">
-            </div>
-        </div>
+        </div> <!-- panel body -->
     </div>
 
 </div>
