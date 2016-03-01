1d6e3dcde033a05161d7798f362c6063d16e991c
work <matuhin.maxim@gmail.com>
2016-03-01 13:46:23 +0900

    UCase

--- config/config.php
+++ config/config.php
@@ -1,22 +1,40 @@
 <?php
 
-return [
-    
+
 //    'mysql' => [
-//        'host' => 'localhost',
-//        'user' => 'webuser',
-//        'pass' => 'webpass',
+//        'host' => '172.28.120.39',
+//        'user' => 'matyukhin',
+//        'pass' => 'ksTg3276sm@',
 //        'base' => 'perco',
 //    ],
-    'mysql' => [
-        'host' => '172.28.120.39',
-        'user' => 'matyukhin',
-        'pass' => 'ksTg3276sm@',
-        'base' => 'perco',
-    ],
-    
-    'dbpath' => '/mnt/prohod/Baza/',
-    //'dbpath' => '/home/matyukhin/db/',
-    //'dbpath' => '/home/fellix/web/www/oper.ngres.ru/www/tests/',
 
-];
+
+class Configuration {
+
+	public static $connection = [
+		'host' => 'localhost',
+		'user' => 'root',
+		'pass' => 'fell1x',
+		'base' => 'perco',
+	];
+
+	public static $scriptList = [
+		'lib/jquery.min',
+		'lib/jquery-ui.min',
+		'lib/jquery.cookie',
+		'lib/bootstrap.min',
+		'lib/moment.min',
+		'lib/i18n/moment-ru',               // rus moment.js
+		'lib/bootstrap-select.min',
+		'lib/bootstrap-datetimepicker.min', // date & time picker
+		'lib/i18n/defaults-ru_RU',          // rus selectpicker
+		'lib/ie10-viewport-bug-workaround', // IE10 viewport hack for Surface/desktop Windows 8 bug
+		'common',
+	];
+
+	public static $dbpath = '/mnt/prohod/Baza';
+
+	public static $siteName  = 'Сервер проходной';
+	public static $brandName = 'PERCo.';
+
+}
